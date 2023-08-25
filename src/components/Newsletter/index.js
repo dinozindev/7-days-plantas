130765e93b096a225aefc2d1236f3d167d80bb02
dinozindev <pugplayzcraft@gmail.com>
2023-08-25 14:20:07 -0300

    finalizando dia 3. Adicionando verificação de email valido a partir do teste do regex

--- src/components/Newsletter/index.js
+++ src/components/Newsletter/index.js
@@ -1,6 +1,7 @@
 import { styled } from "styled-components"
 import { amareloPadrao } from "../Variables"
 import mailIcon from "../../img/mail.svg"
+import { useState } from "react"
 
 const FormContainer = styled.form` 
     width: 70%;     
@@ -29,11 +30,52 @@ const ButtonSend = styled.button`
     
 `
 
+const Erro = styled.span`
+    color: red;
+    padding: 0.5rem 0.5rem;
+`
+
 export default function Newsletter() {
+    
+    // estados de erro e do valor do input de email
+    const [email, setEmail] = useState('');
+    const [error, setError] = useState(null)
+
+    // verificador de email válido a partir de uma regular expression. 
+    function isEmailValid(email) {
+        return /\S+@\S+\.\S+/.test(email);
+    }
+
+    // quando uma atualização for feita no input, atualiza o estado do email. 
+    const handleChange = e => {
+        setEmail(e.target.value)
+    }
+
+    // quando o email for enviado, passa ele como argumento para isEmailValid, verificando se o email é válido a partir do teste com o regex. Caso seja, retorna true. Caso contrário retorna falso e apresenta uma mensagem de erro na tela. 
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        setError(null)
+
+        if(isEmailValid(email)) {
+            console.log("O email é válido")
+        } else {
+            console.log("Email invalido")
+            setError("Email invalido.")
+        }
+    }
+
+
     return (
-        <FormContainer>
-        <NewsletterStyle type="email" placeholder="Insira seu e-mail"/>
+        <FormContainer onSubmit={handleSubmit}>
+        <NewsletterStyle 
+        type="text" 
+        placeholder="Insira seu e-mail"
+        onChange={handleChange}
+        />
         <ButtonSend>Assinar newsletter</ButtonSend>
+        <Erro>{error}</Erro>
         </FormContainer>
+        
     )
 }
